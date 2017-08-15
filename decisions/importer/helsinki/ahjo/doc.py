b0d408fb3bc1e639c45534b791074a5f4e833345
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-08-15 11:05:30 +0300

    Add files for Ahjo XML HTTP/ZIP API
    Add ahjodoc/scanner.py and ahjodoc/doc.py from
    https://github.com/City-of-Helsinki/openahjo/tree/614dc96e91762f762a6e60977251e9ab270ef192

---
+++ decisions/importer/helsinki/ahjo/doc.py
@@ -0,0 +1,438 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import hashlib
+from lxml import etree, html, objectify
+import zipfile
+import logging
+import tempfile
+from pytz import timezone
+from datetime import datetime
+
+local_timezone = timezone('EET')
+
+class ParseError(Exception):
+    pass
+
+def clean_text(text):
+    text = text.replace('\n', ' ')
+    # remove consecutive whitespaces
+    return re.sub(r'\s\s+', ' ', text, re.U).strip()
+
+class AhjoDocument(object):
+    ATTACHMENT_EXTS = ('pdf', 'xls', 'xlsx', 'ppt', 'doc', 'docx', 'png', 'jpg', 'gif', 'txt', 'msg', 'htm')
+
+    def __init__(self, verbosity=1, options={}):
+        self.verbosity = verbosity
+        self.options = options
+        self.logger = logging.getLogger(__name__)
+        pass
+
+    def clean_xml(self):
+        root = self.xml_root
+        REMOVE_LIST = ('Logo', 'MuutoksenhakuohjeetSektio', 'AlatunnisteSektio')
+        for el_name in REMOVE_LIST:
+            el_list = root.xpath("//%s" % el_name)
+            for el in el_list:
+                el.clear()
+
+        def is_empty(el):
+            for val in el.values():
+                if val:
+                    return False
+            if el.text and el.text.strip():
+                return False
+            if el.tail and el.tail.strip():
+                return False
+            if el.getchildren():
+                    return False
+            return True
+
+        def remove_empty_children(el):
+            for ch in el.iterchildren():
+                # Skip the HTML containers
+                if el.tag == 'XHTML':
+                    continue
+                remove_empty_children(ch)
+                if is_empty(ch):
+                    el.remove(ch)
+
+        remove_empty_children(root)
+
+    def parse_header_info(self):
+        desc_info = self.xml_root.xpath('//Kuvailutiedot/JulkaisuMuutostiedot')[0]
+        # WORKAROUND: remove spaces from date and time
+        date = desc_info.find('JulkaisuEsilletulopaiva').text.replace(' ', '')
+        time = desc_info.find('JulkaisuEsilletuloklo').text.replace(' ', '')
+        time_str = "%s %s" % (date, time)
+        self.publish_time = datetime.strptime(time_str, '%Y-%m-%d %H:%M').replace(tzinfo=local_timezone)
+
+        policymaker_el = self.xml_root.xpath('./YlatunnisteSektio/Paattaja')[0]
+        self.policymaker_id = policymaker_el.attrib['PaattajaId']
+
+    def parse_text_section(self, section_el):
+        children = section_el.getchildren()
+        content = []
+        for ch in children:
+            if ch.tag != 'taso1':
+                raise ParseError("Unexpected text section container: %s" % ch.tag)
+            paras = ch.getchildren()
+            for p in paras:
+                if p.tag not in ('Kappale', 'Otsikko', 'Henkilotietoa',
+                                 'Salassapidettava', 'XHTML', 'HenkilotietoaXHTML',
+                                 'Kuva', 'Korjauskappale'):
+                    raise ParseError("Unsupported paragraph tag: %s", p.tag)
+                if p.tag == 'Kappale':
+                    for pch in p.getchildren():
+                        if pch.tag == 'KappaleOptiot':
+                            # FIXME
+                            continue
+                        if pch.tag != 'KappaleTeksti':
+                            raise ParseError("Unsupported paragraph text tag: %s" % pch.tag)
+                        text = pch.text
+                        assert not pch.getchildren()
+                        tail = pch.tail.strip()
+                        assert not tail
+                        content.append(u'<p>%s</p>' % clean_text(text))
+                elif p.tag == 'Otsikko':
+                    if not p.text:
+                        continue
+                    content.append(u'<h3>%s</h3>' % clean_text(p.text))
+                elif p.tag in ('Henkilotietoa', 'HenkilotietoaXHTML'):
+                    content.append(u'<span class="redacted-personal-information">*****</span>')
+                elif p.tag == 'Korjauskappale':
+                    content.append(u'<p class="correction">%s</p>' % clean_text(p.text))
+                elif p.tag == 'Salassapidettava':
+                    content.append(u'<p class="redacted-information">*****</p>')
+                elif p.tag == 'XHTML':
+                    html_str = ''
+                    for pch in p.getchildren():
+                        html_el = html.fromstring(etree.tostring(pch))
+                        etree.strip_tags(html_el, 'font', 'span')
+                        for child_el in html_el.getiterator():
+                            if 'style' in child_el.attrib:
+                                del child_el.attrib['style']
+                        for attr in html_el.attrib:
+                            if attr.startswith('xmlns'):
+                                del html_el.attrib[attr]
+                        s = etree.tostring(html_el, encoding='utf8', method='html')
+                        html_str += clean_text(s.decode('utf8'))
+                    content.append(html_str)
+        return content
+
+    SECTION_TYPES = {
+        'paatosehdotus': 'draft resolution',
+        'paatos': 'resolution',
+        'esitysehdotus': 'draft proposal', # FIXME verify translation
+        'esitys': 'proposal', # FIXME verify translation
+        'lausuntoehdotus': 'draft statement', # FIXME verify translation
+        'lausunto': 'statement', # FIXME verify translation
+        'paatoksenperustelut': 'reasons for resolution',
+        'tiivistelma': 'summary',
+        'esittelija': 'presenter',
+        'kasittely': 'hearing',
+    }
+    def parse_item_content(self, info, item_el):
+        section_el_list = item_el.xpath('./SisaltoSektioToisto/SisaltoSektio')
+        content = []
+        for section_el in section_el_list:
+            s = section_el.attrib.get('sisaltosektioTyyppi')
+            if not s:
+                if info.get('type', None) != 'officeholder_decision':
+                    self.logger.warning("attribute sisaltosektioTyyppi not found")
+                subj_el = section_el.find('SisaltoOtsikko')
+                if subj_el == None:
+                    continue
+                s = subj_el.attrib['SisaltoOtsikkoTyyppi']
+                if not s:
+                    continue
+            if s == '1':
+                s = 'paatos'
+            elif s == '2':
+                s = 'kasittely'
+            if self.verbosity >= 2:
+                self.logger.debug('Section: %s' % s)
+            section_type = self.SECTION_TYPES[s]
+            # sanity check
+            subject = section_el.find('SisaltoOtsikko').text
+            if subject:
+                subject = subject.encode('utf8')
+                if subject.lower() == 'esittelijän perustelut':
+                    subject = 'Esittelijä'
+                elif subject.lower() == 'päätöksen perustelut':
+                    subject = 'paatoksenperustelut'
+                elif subject.lower() == 'päätösehdotus':
+                    subject = 'esitysehdotus'
+                if subject.replace('ä', 'a').replace('ö', 'o').lower() != s:
+                    self.logger.warning("Unexpected section header: %s, expected: %s" % (subject, s))
+                    # In the case of mismatch, override the section name
+                    # based on what kind of document we're parsing (minutes or
+                    # agenda).
+                    if self.type == 'agenda':
+                        if section_type == 'resolution':
+                            section_type = 'draft resolution'
+
+            text_section = section_el.find('TekstiSektio')
+            if not text_section:
+                # If it's an empty content section, skip it.
+                continue
+            paras = self.parse_text_section(text_section)
+            content.append((section_type, paras))
+        info['content'] = content
+
+    def parse_item_attachments(self, info, item_el):
+        att_el_list = item_el.xpath('./LiitteetOptio/Liitteet/LiitteetToisto')
+        att_list = []
+        for att_el in att_el_list:
+            d = {'number': int(att_el.find('Liitenumero').text)}
+            id_el = att_el.find('LiitteetId')
+            att_list.append(d)
+            if id_el is None:
+                d['public'] = False
+                reason_el = att_el.find('SalassapitoOptio')
+                if reason_el is not None:
+                    reason_el = reason_el.find('SalassapidonPerustelut')
+                    if reason_el is not None:
+                        d['confidentiality_reason'] = clean_text(reason_el.text)
+                continue
+            else:
+                d['public'] = True
+            d['id'] = att_el.find('LiitteetId').text
+            d['name'] = att_el.find('Liiteteksti').text
+            for ext in self.ATTACHMENT_EXTS:
+                if d['name'].endswith('.' + ext):
+                    d['name'] = d['name'][:-(1+len(ext))]
+                    break
+        info['attachments'] = att_list
+
+    RESOLUTION_TYPES = {
+        1: 'PASSED',
+        2: 'PASSED_VOTED',
+        3: 'PASSED_REVISED',
+        4: 'PASSED_MODIFIED',
+        5: 'REJECTED',
+        6: 'NOTED',
+        7: 'RETURNED',
+        8: 'REMOVED',
+        9: 'TABLED',
+        10: 'ELECTION',
+    }
+
+    def parse_desc_el(self, item_el, info):
+        desc_el = item_el.find('KuvailutiedotOpenDocument')
+        if not desc_el:
+            raise ParseError("Field KuvailutiedotOpenDocument missing")
+
+        doc_type = desc_el.find('AsiakirjaTyyppi')
+        if doc_type is not None:
+            doc_type = doc_type.text
+        if doc_type == u'päätös':
+            info['type'] = 'decision'
+        elif doc_type == u'esitys':
+            info['type'] = 'proposal'
+        elif doc_type == u'viranhaltijan päätös':
+            info['type'] = 'officeholder_decision'
+        elif doc_type == u'luottamushenkilön päätös':
+            info['type'] = 'trustee_decision'
+        elif doc_type:
+            raise ParseError("Invalid item type: %s" % doc_type)
+
+        lang_el = desc_el.find('Kieli')
+        if lang_el is not None:
+            lang_id = lang_el.attrib['KieliID']
+        else:
+            # Sometimes the language field can go missing...
+            lang_id = 'fi-FI'
+        if lang_id != 'fi-FI':
+            print "Invalid language: %s" % lang_id
+            return False
+        register_id_el = desc_el.find('Dnro')
+
+        subject = clean_text(desc_el.find('Otsikko').text)
+        if subject.startswith('V '):
+            # Strip the first word
+            subject = subject.split(' ', 1)[1]
+            # Strip possible date
+            subject = re.sub(r'^\d+\.\d+\.\d+', '', subject)
+            # Strip comma and space
+            subject = re.sub(r'^[, ]+', '', subject)
+        # Strip Kj/Ryj/Kaj...
+        subject = re.sub(r'^\w{2,4} ?/ *', '', subject)
+        info['subject'] = subject
+
+        if self.verbosity >= 2:
+            self.logger.debug('Parsing item: %s' % info['subject'])
+
+        if register_id_el:
+            info['register_id'] = register_id_el.find('DnroLyhyt').text.strip()
+        else:
+            info['register_id'] = None
+
+        cat_el = desc_el.find('Tehtavaluokka')
+        if cat_el is not None:
+            info['category'] = cat_el.text.strip()
+        else:
+            info['category'] = None
+
+        references = desc_el.find('Viite')
+        if references is not None:
+            info['reference_text'] = references.text.strip()
+
+        raw_document_classification = desc_el.find('AsiakirjallinenTieto')
+        if raw_document_classification is not None:
+            m = re.match(r'([0-9 -]+) (\D+)', raw_document_classification.text.strip())
+            if m:
+                id_no, description = m.groups()
+                info['classification_code'] = id_no
+                info['classification_description'] = description
+
+        kw_list = []
+        for kw_el in desc_el.findall('Asiasanat'):
+            kw_list.append(clean_text(kw_el.text))
+        info['keywords'] = kw_list
+
+        resolution_el = desc_el.find('Pikapaatos')
+        if resolution_el is not None:
+            info['resolution'] = self.RESOLUTION_TYPES[int(resolution_el.attrib['PikapaatosId'])]
+
+        introducer_el = desc_el.find('Esittelija')
+        if introducer_el is not None:
+            info['introducer'] = introducer_el.text.strip()
+        preparer_el = desc_el.find('Valmistelija')
+        if preparer_el is not None:
+            info['preparer'] = preparer_el.text.strip()
+        return True
+
+    def parse_item(self, index, item_el, is_top_level=True):
+        info = {}
+        if self.verbosity >= 3:
+            self.logger.debug(etree.tostring(item_el, encoding='utf8', method='html'))
+
+        if is_top_level:
+            if not self.parse_desc_el(item_el, info):
+                return None
+
+        item_nr = index + 1
+        info['number'] = item_nr
+
+        self.parse_item_content(info, item_el)
+        self.parse_item_attachments(info, item_el)
+
+        # If we're parsing a minutes entry, find the agenda item entry in order
+        # to get better decision text content.
+        if is_top_level and self.type == 'minutes':
+            agenda_doc_el = item_el.find('Asiakirja')
+            if agenda_doc_el:
+                ai_content = self.parse_item(index, agenda_doc_el, False)['content']
+                for content_type, content in ai_content:
+                    if content_type == 'draft resolution':
+                        continue
+                    # If we don't have the content type already, augment it
+                    # with the content from the agenda document.
+                    for c in info['content']:
+                        if c[0] == content_type:
+                            break
+                    else:
+                        info['content'].append((content_type, content))
+
+        return info
+
+    def parse_from_xml(self, xml_str):
+        self.xml_root = etree.fromstring(xml_str)
+        if self.xml_root.tag == 'Esityslista':
+            self.type = "agenda"
+        elif self.xml_root.tag == 'Poytakirja':
+            self.type = "minutes"
+        else:
+            raise ParseError("Invalid root tag in XML file: %s" % self.xml_root.tag)
+        self.clean_xml()
+        self.parse_header_info()
+
+        if self.type == "minutes":
+            item_container = 'Paatokset'
+        else:
+            item_container = 'Esitykset'
+        self.items = []
+        item_els = self.xml_root.xpath('./%s/Asiakirja' % item_container)
+        for idx, item_el in enumerate(item_els):
+            info = self.parse_item(idx, item_el)
+            if not info:
+                continue
+            self.items.append(info)
+
+    def output_cleaned_xml(self, out_file):
+        s = etree.tostring(self.xml_root, encoding='utf8')
+        out_file.write(s)
+
+    def import_from_zip(self, in_file):
+        zipf = zipfile.ZipFile(in_file)
+        name_list = zipf.namelist()
+        xml_names = [x for x in name_list if x.endswith('.xml')]
+        if len(xml_names) == 0:
+            # FIXME: Workaround for a silly bug
+            xml_names = [x for x in name_list if re.match(r'\w+\d+xml_', x)]
+            if len(xml_names) != 1:
+                raise ParseError("No XML file in Ahjo ZIP file")
+        if len(xml_names) > 1:
+            raise ParseError("Too many XML files in Ahjo ZIP file")
+        xml_file = zipf.open(xml_names[0])
+        xml_s = xml_file.read()
+        xml_file.close()
+        self.parse_from_xml(xml_s)
+        self.zip_file = zipf
+
+    def extract_zip_attachment(self, att, out_path):
+        BLOCK_SIZE = 1*1024*1024
+
+        name_list = self.zip_file.namelist()
+        names = [x for x in name_list if att['id'] in x]
+        if not len(names):
+            raise ParseError("Attachment %s not found in ZIP file" % att['id'])
+        zip_info = self.zip_file.getinfo(names[0])
+
+        att_file = self.zip_file.open(names[0])
+        sha1 = hashlib.new('sha1')
+        self.logger.info("Hashing attachment '%s'" % att['name'])
+        while True:
+            block = att_file.read(BLOCK_SIZE)
+            if not block:
+                break
+            sha1.update(block)
+        att['hash'] = sha1.hexdigest()
+        att_file.close()
+
+        ext = names[0].split('.')[-1].lower()
+        if ext not in self.ATTACHMENT_EXTS:
+            # Workaround for an ugly bug
+            for e in self.ATTACHMENT_EXTS:
+                arr = ext.split('_')
+                if len(arr) >= 2 and ext.split('_')[-2].endswith(e):
+                    ext = e
+                    break
+
+            if ext not in self.ATTACHMENT_EXTS:
+                raise ParseError("Unknown attachment type (%s)" % ext)
+        att['type'] = ext
+        f_name = '%s.%s' % (att['hash'], ext)
+        subdir = att['hash'][0:2]
+        att['file'] = os.path.join(subdir, f_name)
+        storage_dir = os.path.join(out_path, subdir)
+        if not os.path.exists(storage_dir):
+            os.makedirs(storage_dir)
+        f_path = os.path.join(storage_dir, f_name)
+        if os.path.exists(f_path):
+            if not self.options.get('ignore-attachment-size', False):
+                if os.path.getsize(f_path) != zip_info.file_size:
+                    raise ParseError("Size mismatch with attachment '%s': %d vs. %d" % (f_name, os.path.getsize(f_path), zip_info.file_size))
+            self.logger.info("Skipping existing attachment '%s'" % f_name)
+            return
+        self.logger.info("Extracting attachment '%s' to '%s'" % (att['name'], f_name))
+        att_file = self.zip_file.open(names[0])
+        out_file = open(f_path, 'w')
+        while True:
+            block = att_file.read(BLOCK_SIZE)
+            if not block:
+                break
+            out_file.write(block)
+        out_file.close()
+        att_file.close()
---
+++ decisions/importer/helsinki/ahjo/scanner.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+import sys
+import os
+import tempfile
+import shutil
+import requests
+import requests_cache
+import logging
+from datetime import datetime
+from lxml import html
+from progressbar import ProgressBar
+from .doc import local_timezone
+
+SKIP_DOC_LIST = [
+    'Opev_SKJ_2013-2_El',
+    'HKR_Ytlk_2013-18_El',
+    'Ymk_Ylk_2013-1_El',
+    'Ork_Orkjk_2014-4_Pk',  # HEL 2014-011315 processed twice
+    'Rakpa_Tplk_2013-1_Pk',  # KuvailutiedotOpenDocument missing
+    'Halke_Khs_2012-27_Pk',  # Attachment missing
+    'Halke_Khs_2012-26_Pk',  # Attachment missing
+    'HKL_75001VH1_2016-44_Pk',
+    'Opev_4009211VH1_2015-18_Pk',  # Invalid attachment
+    'Rakpa_Tplk_2014-11_Pk',  # KuvailutiedotOpenDocument missing
+    'Kymp_U51105100VH1_2017-21_Pk',  # KuvailutiedotOpenDocument missing
+]
+
+SKIP_URL_LIST = [
+    'http://openhelsinki.hel.fi/files/Taloushallintopalvelu-liikelaitoksen%20jk_71900/Talpa%202013-05-28%20Talpajk%203%20El%20Su.zip', # Duplicate
+    'http://openhelsinki.hel.fi/files/Kaupunginhallituksen%20konsernijaosto_02978/Halke%202013-08-26%20Koja%2011%20El%20Su.zip', # Wrong meeting id
+    'http://openhelsinki.hel.fi/files/Kaupunginhallituksen%20konsernijaosto_02978/Halke%202013-08-26%20Koja%2011%20Pk%20Su.zip', # Wrong meeting id
+    'http://openhelsinki.hel.fi/files/Kaupunginmuseon%20johtokunta_46113/Museo%202013-08-27%20Museojk%207%20El%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Kaupunginmuseon%20johtokunta_46113/Museo%202013-08-27%20Museojk%207%20Pk%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Suomenkielisen%20tyovaenopiston%20jk_45100/Sto%202013-08-27%20Stojk%2012%20El%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Suomenkielisen%20tyovaenopiston%20jk_45100/Sto%202013-08-27%20Stojk%2012%20Pk%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Taloushallintopalvelu-liikelaitoksen%20jk_71900/Talpa%202013-11-26%20Talpajk%205%20El%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Keskusvaalilautakunta_11500/Kanslia%202014-09-02%20Kvlk%207%20El%20Su.zip', # wrong date
+    'http://openhelsinki.hel.fi/files/Liikuntalautakunta_47100/Liv%202014-10-23%20LILK%2012%20El%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Pelastuslautakunta_11100/Pel%202014-06-10%20PELK%207%20El%20Su.zip', # corrupt
+    'http://openhelsinki.hel.fi/files/Liikuntalautakunta_47100/Liv%202014-10-23%20LILK%2011%20El%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Liikuntalautakunta_47100/Liv%202014-10-07%20LILK%2015%20El%20Su.zip', # wrong meeting id
+    'http://openhelsinki.hel.fi/files/Taidemuseo_46102/Museonjohtaja_46102VH1/Taimu%202104-10-10%2046102VH1%2026%20Pk%20Su.zip', # wrong date
+    'http://openhelsinki.hel.fi/files/Rakennusvirasto_52000/Tulosryhman%20johtaja_521112VH1/HKR%202014-12-16%20521112VH1%2012%20Pk%20Su.zip', # wrong date
+    'http://openhelsinki.hel.fi/files/Sosiaali-%20ja%20terveyslautakunta_81000/Sote%202013-06-04%20Sotelk%209%20Pk%20Su.zip', # missing attachment
+    'http://openhelsinki.hel.fi/files/Sosiaali-%20ja%20terveyslautakunta_81000/Sote%202013-06-04%20Sotelk%209%20El%20Su.zip', # missing attachment
+]
+
+
+CHUNK_SIZE = 32*1024
+
+URL_BASE = "http://openhelsinki.hel.fi"
+
+
+class AhjoScanner(object):
+    def __init__(self, verbosity=1):
+        self.verbosity = verbosity
+        self.doc_store_path = None
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+
+    def scan_dir(self, path, policymaker_id):
+        if path.endswith('.zip/'):
+            self.logger.warning("Directory ends with .zip: %s" % path)
+            return []
+        r = requests.get(URL_BASE + path)
+        if r.status_code != 200:
+            raise Exception("Failed to read directory '%s'" % path)
+        root = html.fromstring(r.content)
+        links = root.xpath("//a")
+        info_list = []
+        for link_el in links:
+            link = link_el.attrib['href']
+            if path in link and link.endswith('/'):
+                # Is a sub-directory
+                new_policymaker_id = link.strip('/').split('_')[-1].strip()
+                sub_list = self.scan_dir(link, new_policymaker_id)
+                info_list += sub_list
+                continue
+
+            if not link.endswith('.zip'):
+                continue
+
+            fname = link.split('/')[-1]
+            fname = fname.replace('.zip', '')
+            FIELD_NAMES = ("org", "date", "policymaker", "meeting_nr", "doc_type_id", "language")
+            fields = fname.split('%20')
+            info = {}
+            if len(fields) == len(FIELD_NAMES) - 1:
+                self.logger.warning("Language field missing in %s" % ' '.join(fields))
+                fields.append('Su')
+            if len(fields) != len(FIELD_NAMES):
+                self.logger.warning("Invalid filename: %s" % fname)
+                continue
+            for idx, f in enumerate(FIELD_NAMES):
+                info[f] = fields[idx]
+            info['meeting_nr'] = int(info['meeting_nr'])
+            info['year'] = int(info['date'].split('-')[0])
+            info['policymaker_id'] = policymaker_id
+            info['policymaker_abbr'] = info['policymaker']
+            # Skip Swedish documents
+            if info['language'] != 'Su':
+                continue
+
+            DOC_TYPES = {'Pk': 'minutes', 'El': 'agenda'}
+            info['doc_type'] = DOC_TYPES[info['doc_type_id']]
+
+            # Fetch timestamp from directory listing
+            elems = link_el.getprevious().tail.split('   ')
+            ts_text = elems[0].strip()
+            file_size = int(elems[-1].strip())
+            time = datetime.strptime(ts_text, "%m/%d/%Y %I:%M %p")
+            time = time.replace(tzinfo=local_timezone)
+            info['last_modified'] = time
+
+            info['url'] = URL_BASE + link
+            if file_size < 500:
+                self.logger.warning("File too small: %s %d" % ((URL_BASE + link), file_size))
+                continue
+            info['origin_id'] = self.generate_doc_id(info)
+            if info['url'] in SKIP_URL_LIST:
+                self.logger.warning("Skipping document on URL skip list: %s" % info['origin_id'])
+                continue
+            if info['origin_id'] in SKIP_DOC_LIST:
+                self.logger.warning("Skipping document on skip list: %s" % info['origin_id'])
+                continue
+
+            info_list.append(info)
+        return info_list
+
+    def scan_documents(self, cached=False):
+        if cached:
+            requests_cache.install_cache()
+        r = requests.get(URL_BASE + '/files/')
+        if r.status_code != 200:
+            raise Exception("Directory read failed")
+        root = html.fromstring(r.content)
+        links = root.xpath("//a")
+        info_list = []
+        for link_el in links:
+            link = link_el.attrib['href']
+            if not link.startswith('/files'):
+                continue
+            policymaker_id = link_el.text.split('_')[-1].strip()
+            dir_list = self.scan_dir(link, policymaker_id)
+            info_list = info_list + dir_list
+        self.doc_list = info_list
+        if cached:
+            requests_cache.uninstall_cache()
+        return info_list
+
+    def generate_doc_id(self, info):
+        s = "%s_%s_%d-%d_%s" % (info['org'], info['policymaker'], info['year'], info['meeting_nr'], info['doc_type_id'])
+        return s
+
+    def download_document(self, info):
+        self.logger.info('Fetching document from %s' % info['url'])
+
+        if self.doc_store_path:
+            try:
+                os.makedirs(self.doc_store_path)
+            except OSError:
+                pass
+            fname = "%s_%s.zip" % (info['date'], self.generate_doc_id(info))
+            store_fpath = os.path.join(self.doc_store_path, fname)
+            if os.path.exists(store_fpath):
+                mtime = datetime.fromtimestamp(os.path.getmtime(store_fpath))
+                mtime = mtime.replace(tzinfo=local_timezone)
+                # Use stored file only if it's newer.
+                if mtime >= info['last_modified']:
+                    return open(store_fpath, 'rb')
+            delete_tmp = False
+        else:
+            store_fpath = None
+            delete_tmp = True
+
+        outf = tempfile.NamedTemporaryFile(mode="w+b", delete=delete_tmp)
+
+        resp = requests.get(info['url'], stream=True)
+        total_len = int(resp.headers['content-length'])
+        if sys.stdout.isatty():
+            pbar = ProgressBar(maxval=total_len).start()
+        else:
+            pbar = None
+        bytes_down = 0
+        for chunk in resp.iter_content(CHUNK_SIZE):
+            outf.write(chunk)
+            bytes_down += len(chunk)
+            if pbar:
+                pbar.update(bytes_down)
+        if pbar:
+            pbar.finish()
+        if store_fpath:
+            outf.close()
+            shutil.move(outf.name, store_fpath)
+            outf = open(store_fpath, 'rb')
+        else:
+            outf.seek(0)
+        return outf
+
+if __name__ == "__main__":
+    scanner = AhjoScanner()
+    logging.basicConfig()
+    scanner.doc_store_path = ".cache"
+
+    doc_list = scanner.scan_documents()
+
+    for doc in doc_list:
+        scanner.download_document(doc)
