678a646db04d584c7865e6027d39ce34c77d23ff
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-08-31 12:59:10 +0300

    ahjo: Implement importing to database

--- decisions/importer/helsinki/ahjo/db_importer.py
+++ decisions/importer/helsinki/ahjo/db_importer.py
@@ -2,7 +2,9 @@ import logging
 
 from django.db import transaction
 
-from ....models import DataSource, ImportedFile
+from ....models import (
+    Action, Case, Content, DataSource, Event, Function, ImportedFile,
+    Organization, OrganizationClass, Person)
 from .importer import ChangeImporter
 
 LOG = logging.getLogger(__name__)
@@ -49,3 +51,152 @@ class DatabaseImporter(ChangeImporter):
         """
         LOG.info("Updating data from %s", doc_info.origin_id)
         doc = doc_info.get_document()
+        self._import_document(doc_info, doc)
+
+    def _import_document(self, doc_info, doc):
+        event = self._import_event(doc_info, doc)
+        self._import_attendees(doc, event)
+        self._import_actions(doc, event)
+
+    def _import_event(self, doc_info, doc):
+        policymaker_id = doc_info.policymaker_id
+        defaults = {
+            'name': doc.event.name,
+            'start_date': doc.event.start_date,
+            'end_date': doc.event.end_date,
+            'organization': self._get_or_create_organization(policymaker_id),
+        }
+        (event, created) = Event.objects.update_or_create(
+            data_source=self.data_source,
+            origin_id=doc_info.origin_id,
+            defaults=defaults)
+        _log_update_or_create(event, created)
+        return event
+
+    def _get_or_create_organization(self, policymaker_id):
+        (organization, created) = Organization.objects.get_or_create(
+            origin_id=policymaker_id, defaults={
+                'name': str(policymaker_id),
+                'classification': self._dummy_organization_class,
+            })
+        _log_update_or_create(organization, created)
+        return organization
+
+    @property
+    def _dummy_organization_class(self):
+        if not hasattr(self, '_cached_dummy_organization_class'):
+            (org_class, created) = OrganizationClass.objects.get_or_create(
+                name='dummy')
+            _log_update_or_create(org_class, created)
+            self._cached_dummy_organization_class = org_class
+        return self._cached_dummy_organization_class
+
+    def _import_attendees(self, doc, event):
+        imported_attendees = set()
+        for attendee_data in (doc.event.attendees or []):
+            defaults = {
+                'role': attendee_data.role,
+            }
+            (attendee, created) = event.attendees.update_or_create(
+                data_source=self.data_source,
+                person=self._get_or_create_person(attendee_data),
+                defaults=defaults)
+            _log_update_or_create(attendee, created)
+            imported_attendees.add(attendee.pk)
+
+        # Delete all old non-updated attendees (if there is any)
+        event.attendees.exclude(pk__in=imported_attendees).delete()
+
+    def _get_or_create_person(self, data):
+        names = data.name.split(None, 1) or ['']
+        first_name = names[0] if len(names) >= 2 else ''
+        last_name = names[-1]
+        (person, created) = Person.objects.get_or_create(
+            data_source=self.data_source,
+            origin_id='{name}/{title}'.format(
+                name=data.name, title=(data.title or '')),
+            defaults={
+                'name': data.name,
+                'given_name': first_name,
+                'family_name': last_name,
+            })
+        if created:
+            _log_update_or_create(person, created)
+        return person
+
+    def _import_actions(self, doc, event):
+        imported_actions = set()
+        for (num, action_data) in enumerate(doc.event.actions or []):
+            case = self._import_case(action_data, event, num)
+            action = self._import_action(action_data, event, num, case)
+            imported_actions.add(action.pk)
+            self._import_contents(action_data, action)
+
+        # Delete all old non-updated actions (if there is any)
+        event.actions.exclude(pk__in=imported_actions).delete()
+
+    def _import_case(self, action_data, event, num):
+        if not action_data.register_id:
+            return None
+        defaults = {
+            'title': action_data.title,
+            'function': self._get_or_create_function(action_data),
+        }
+        (case, created) = Case.objects.update_or_create(
+            data_source=self.data_source,
+            register_id=action_data.register_id,
+            defaults=defaults)
+        _log_update_or_create(case, created)
+        self._import_attachments(action_data, case)
+        return case
+
+    def _get_or_create_function(self, data):
+        if not data.function_id:
+            return None
+        (function, created) = Function.objects.get_or_create(
+            data_source=self.data_source,
+            function_id=data.function_id,
+            defaults={'name': data.function_name})
+        if created:
+            _log_update_or_create(function, created)
+        return function
+
+    def _import_attachments(self, action_data, case):
+        for attachemnt_data in (action_data.attachments or []):
+            pass  # TODO: Import attachments
+
+    def _import_action(self, action_data, event, num, case):
+        defaults = {
+            'case': case,
+            'title': action_data.title,
+            'ordering': num,
+            'resolution': action_data.resolution or '',
+            'event': event,
+            'article_number': str(action_data.article_number or ''),
+        }
+        (action, created) = Action.objects.update_or_create(
+            data_source=self.data_source,
+            origin_id='{event.origin_id}:{num}'.format(event=event, num=num),
+            defaults=defaults)
+        _log_update_or_create(action, created)
+        return action
+
+    def _import_contents(self, action_data, action):
+        defaults = {
+            'hypertext': action_data.content,
+            'type': 'decision',  # XXX
+            'ordering': 1,
+        }
+        (content, created) = Content.objects.update_or_create(
+            data_source=self.data_source,
+            action=action,
+            origin_id=action.origin_id,
+            defaults=defaults)
+        _log_update_or_create(content, created)
+
+
+def _log_update_or_create(obj, created):
+    if created:
+        LOG.info('Created %s %s', type(obj).__name__, obj.pk)
+    else:
+        LOG.info('Updated %s %s', type(obj).__name__, obj.pk)
