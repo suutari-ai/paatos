774c8c8a9769825c9532eae3ec11900ccf2af83f
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-08-31 12:54:42 +0300

    ahjo: Document and improve importing

--- decisions/importer/helsinki/ahjo/db_importer.py
+++ decisions/importer/helsinki/ahjo/db_importer.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+
 from ....models import ImportedFile
 from .importer import ChangeImporter
 
@@ -9,19 +11,32 @@ class DatabaseImporter(ChangeImporter):
     def __init__(self, data_source):
         self.data_source = data_source
 
-    def get_imported_version(self, path):
+    def should_import(self, doc_info):
+        # Currently only "minutes" are imported, "agenda" is not.
+        should_import = (doc_info.doc_type == 'minutes')
+        return should_import
+
+    def _import_single(self, doc_info):
+        with transaction.atomic():
+            super(DatabaseImporter, self)._import_single(doc_info)
+
+    def get_imported_version(self, doc_info):
         imported_file = ImportedFile.objects.filter(
-            data_source=self.data_source, path=path).first()
+            data_source=self.data_source, path=doc_info.path).first()
         return imported_file.imported_version if imported_file else None
 
-    def set_imported_version(self, path, version):
-        (imported_file, created) = ImportedFile.objects.get_or_create(
-            data_source=self.data_source, path=path, defaults={
+    def set_imported_version(self, doc_info, version):
+        ImportedFile.objects.update_or_create(
+            data_source=self.data_source, path=doc_info.path, defaults={
                 'imported_version': version})
-        if not created:
-            imported_file.imported_version = version
-            imported_file.save(update_fields=['imported_version'])
 
     def handle_document_changed(self, doc_info):
+        """
+        Handle document change or a new document.
+
+        Import or update the data from the document to the database.
+
+        :type doc_info: .docinfo.DocumentInfo
+        """
         print("Updating data from {}".format(doc_info))
         doc = doc_info.get_document()
